5f1f091b5aef4ff08793554be0a52ad3d5db2a08
sejal.parikh <sejal.parikh@salesforce.com>
2021-12-30 00:47:14 -0500

    back to new sandbox, and removing unwanted translations

--- app/commerce-api.config.js
+++ app/commerce-api.config.js
@@ -1,8 +1,8 @@
 export const commerceAPIConfig = {
     proxyPath: `/mobify/proxy/api`,
     parameters: {
-        clientId: 'c9c45bfd-0ed3-4aa2-9971-40f88962b836',
-        organizationId: 'f_ecom_zzrf_001',
+        clientId: '1d763261-6522-4913-9d52-5d947d3b94c4',
+        organizationId: 'f_ecom_zzte_053',
         shortCode: 'kv7kzm78',
         siteId: 'RefArch'
     }
